ad569e9e13997036c11472744dd41268006c409b
jettzheng <jettzheng0825@outlook.com>
2020-03-03 11:15:06 +0800

    test

---
+++ handlers/hello.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(rw http.RespnseWriter, r *http.Request) {
+	h.l.Println("Hello World")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw,"OOps",http.StatusBadRequest)
+	}
+	fmt.Fprintf(rw, "Hello %s", d)
+}
