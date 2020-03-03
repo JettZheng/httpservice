ea5ec251de3cd08bc359cc588aafee6699f4c4b9
jettzheng <jettzheng0825@outlook.com>
2020-03-03 11:21:10 +0800

    test

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module github.com/JettZheng/httpservice/api
 
 go 1.13
+
+require github.com/JettZheng/httpservice v0.0.0-20200303031506-ad569e9e1399 // indirect
--- handlers/hello.go
+++ handlers/hello.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -17,7 +19,7 @@ func (h *Hello) ServeHTTP(rw http.RespnseWriter, r *http.Request) {
 	h.l.Println("Hello World")
 	d, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		http.Error(rw,"OOps",http.StatusBadRequest)
+		http.Error(rw, "OOps", http.StatusBadRequest)
 	}
 	fmt.Fprintf(rw, "Hello %s", d)
 }
