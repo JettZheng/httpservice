043501ee16a88f28859bbdaaf84a17a1cd321459
jettzheng <jettzheng0825@outlook.com>
2020-03-03 11:10:41 +0800

    test

--- api/main.go
+++ api/main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/JettZheng/httpservice/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw,"OOps",http.StatusBadRequest)
-			//rw.WriteHeader(http.StatusBadRequest)
-			//rw.Write([]byte("Ooops"))
-		}
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	l := log.New(os.Stdout, "api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
-	http.ListenAndServe("localhost:9090", nil)
+	sm := http.NewServeMux()
+	sm.Handle("/",hh)
+
+	http.HandleFunc( )
+	http.ListenAndServe("localhost:9090", sm)
 }
